eae74ebfe15b29e0a9fc1a091082a2d5f180a854
Benjamin Lodi <brlodi@gmail.com>
2016-01-31 18:36:27 -0500

    Add basic install script

---
+++ symlink_dotfiles.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# symlink_dotfiles.sh
+
+ln -sv .aliases ~/
+ln -sv .bash_profile ~/
+ln -sv .bash_prompt ~/
+ln -sv .exports ~/
+ln -sv .extra ~/
+ln -sv .functions ~/
+ln -sv .gitconfig ~/
+ln -sv .inputrc ~/
