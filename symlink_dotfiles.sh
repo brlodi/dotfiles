01f15e85240246baa918dff1f3c8bb3ddeff22ac
Benjamin Lodi <brlodi@gmail.com>
2016-02-03 21:00:11 -0500

    "Improved" symlink script by making it remove editing files without warning

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 # symlink_dotfiles.sh
 
+# Warn that we'll be overwiting any existing dotfiles
+# TODO
+
+# Remove existing files/links. This is super not-safe right now
+rm ~/.aliases
+rm ~/.bash_profile
+rm ~/.bash_prompt
+rm ~/.exports
+rm ~/.extra
+rm ~/.functions
+rm ~/.gitconfig
+rm ~/.inputrc
+
+# Place symlinks in ~
 ln -sv .aliases ~/
 ln -sv .bash_profile ~/
 ln -sv .bash_prompt ~/
