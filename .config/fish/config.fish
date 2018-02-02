987d9f2aee88f4a8136e4b495fdf14c9e8a3ecd1
Benjamin Lodi <brlodi@gmail.com>
2018-02-02 16:49:03 -0500

    Integrate Base16-Shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,6 +20,10 @@ end
 # Turn off path name truncation (by default shows only first letter)
 set -U fish_prompt_pwd_dir_length 0
 
+# Set terminal colors using Base16-Shell, and also export the Base16 colors as
+# hex codes for BobTheFish to use below
+base16_set_theme eighties
+
 
 # ----------------------------- BobTheFish theme ---------------------------- #
 
@@ -30,28 +34,30 @@ set -g theme_show_exit_status yes
 
 set -g theme_color_scheme user
 
-set -g __color_initial_segment_exit  brgreen red --bold
-set -g __color_initial_segment_su    brgreen green --bold
-set -g __color_initial_segment_jobs  brgreen blue --bold
+set -g __color_initial_segment_exit  $__theme_base00 $__theme_base08 --bold
+set -g __color_initial_segment_su    $__theme_base00 $__theme_base0C --bold
+set -g __color_initial_segment_jobs  $__theme_base00 $__theme_base0D --bold
 
-set -g __color_username              brgreen green --bold
-set -g __color_hostname              brgreen blue
+set -g __color_username              $__theme_base02 $__theme_base0B
+set -g __color_hostname              $__theme_base02 $__theme_base0C --bold
 
-set -g __color_path                  bryellow white
-set -g __color_path_basename         bryellow white --bold
-set -g __color_path_nowrite          bryellow white
-set -g __color_path_nowrite_basename bryellow red --bold
+set -g __color_path                  $__theme_base01 $__theme_base05
+set -g __color_path_basename         $__theme_base01 $__theme_base05 --bold
+set -g __color_path_nowrite          $__theme_base01 $__theme_base05
+set -g __color_path_nowrite_basename $__theme_base01 $__theme_base08 --bold
 
-set -g __color_repo                  brgreen brred
-set -g __color_repo_work_tree        brred black --bold
-set -g __color_repo_dirty            brgreen red
-set -g __color_repo_staged           brgreen yellow
+set -g __color_repo                  $__theme_base02 $__theme_base0F
+set -g __color_repo_work_tree        $__theme_base06 $__theme_base08 --bold
+set -g __color_repo_dirty            $__theme_base02 $__theme_base09
+set -g __color_repo_staged           $__theme_base02 $__theme_base09
 
-set -g __color_vi_mode_default       green black --bold
-set -g __color_vi_mode_insert        blue black --bold
-set -g __color_vi_mode_visual        magenta black --bold
+set -g __color_vi_mode_default       $__theme_base0B $__theme_base00 --bold
+set -g __color_vi_mode_insert        $__theme_base0D $__theme_base00 --bold
+set -g __color_vi_mode_visual        $__theme_base0E $__theme_base00 --bold
 
-set -g __color_vagrant               brgreen white --bold
-set -g __color_rvm                   brgreen red --bold
-set -g __color_virtualfish           brgreen cyan --bold
-set -g __color_virtualgo             brgreen cyan --bold
+set -g __color_vagrant               $__theme_base02 $__theme_base0A --bold
+
+set -g __color_rvm                   $__theme_base08 $__theme_base00 --bold
+set -g __color_virtualfish           $__theme_base0C $__theme_base00 --bold
+set -g __color_virtualgo             $__theme_base0C $__theme_base00 --bold
+
