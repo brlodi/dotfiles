b05689deebe4d3a84f57730ea751f36d68c25585
Benjamin Lodi <brlodi@gmail.com>
2018-02-02 17:26:50 -0500

    Also set vim colorscheme with base16-shell

--- .config/fish/functions/base16_set_theme.fish
+++ .config/fish/functions/base16_set_theme.fish
@@ -1,15 +1,16 @@
-function base16_set_theme -d "Set terminal colors with Base16-Shell and output named variables"
+function base16_set_theme -d "Set terminal colors with base16-shell"
   set -l theme $argv
   set -l base16_shell_dir "$HOME/.config/base16-shell"
   set -l base16_theme_script "$base16_shell_dir/scripts/base16-$theme.sh"
   set -l colortest "$base16_shell_dir/colortest"
-  if test -e {$base16_theme_script}
+  if status --is-interactive; and test -e {$base16_theme_script}
     eval sh {$base16_theme_script}
     set -l colortest_output (eval $colortest "base16-$theme.sh")
     for line in $colortest_output
       set -l varname (string match -r "base[a-fA-F0-9]{2}" $line)
       set -g "__theme_$varname" (string match -r "[a-fA-F0-9]{6}" $line)
     end
+    echo -e "colorscheme base16-$theme\nlet g:airline_theme='base16_shell'" > ~/.vim_colors
   else
     # Establish named 16-color fallbacks for BobTheFish to use. This won't
     # look very good, but it at least won't break the prompt
